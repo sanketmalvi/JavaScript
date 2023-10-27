b2a73467542eca3e6e5652ac4a0fa8cebd6b68d3
Sanket Malvi <sanketmalvi5@gmail.com>
2023-10-27 13:53:39 +0530

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 145533
+let accountEmail = "sanket@gmail.com"
+var accountPass = "1234"
+accountCity = "Indore"
+let accountState;
+
+// accountId = 456666
+
+accountEmail = "sm@gmail.com"
+accountPass = "9746"
+accountCity = "Betul"
+
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
+// Use Console.table when you have to print multiple variables
+
+
+
+/*
+Prefer not to use var, instead of use let
+beacuse of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict";  // It treat all JS as newer version
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log(3 
+    +
+    3)      // Code readability should be high
+    
+console.log("Sanket");
+
+// -----------------------------------------------------------------
+
+
+
+let name = "sanket"
+let age = 22
+let isLoggedIn = false
+
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stanalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof "Sanket");
+console.log(typeof age);
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
