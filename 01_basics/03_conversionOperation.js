c232dddc095b50c6deaafc196da03898486b97d0
Sanket Malvi <sanketmalvi5@gmail.com>
2023-10-27 14:49:47 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+// let score = "33"
+// let score = "33abc"
+// let score = true
+let score = null
+// let score = undefined
+
+
+// console.log(score);
+// console.log((score));  //as a method
+
+let valueInNumber = Number(score)  //CONVERT VARIABLE IN NUMBER
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan (Not a Number)
+// true => 1; false => 0
+
+
+
+// ----------------------------------------------------------------------------------------------------
+
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)  //  CONVERT IN BOOLEAN
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" = false
+// "sanket" = true
+
+
+
+
+// ----------------------------------------------------------------------------------------------------
+
+
+
+let aNumber = 35
+// let aNumber = true
+
+let stringNumber = String(aNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+// ----------------------------------------------------------------------------------------------------
