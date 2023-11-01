f4a624139d7fc5625efd4fee3dcc22e2bf9c6a62
Sanket Malvi <sanketmalvi5@gmail.com>
2023-11-01 18:04:10 +0530

    Stack and Heap

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,78 @@
+/*
+# Primitive Data Types - (Call by value)
+
+7 TYPES : String, number, boolean, null, undefined, symbol, BigInt
+*/
+
+const score = 100  //number
+const scoreValue = 100.3  //number
+
+const isLoggedIn = false // boolean
+const outsideTemp = null //object
+let userEmail;  //undefined
+
+
+const id = Symbol('123')   
+const anotherId = Symbol('123')  //symbol
+
+// console.log(id === anotherId);  //false
+// console.log(typeof id);
+
+/*
+# Non-Primitive Data Types - (Reference)
+
+Arrays, Objects, Functions
+*/
+
+const heros = ["shaktiman", "Krish", "Flyingjatt"];   //array
+
+
+let myObj = {
+    name: "Jack",             //Objects : All values in the braces 
+    age: "20",
+    add: "mumbai"
+}
+
+// Function
+const myFunction = function(){
+    // console.log("Helloworld")
+}
+// console.log(myFunction);
+// console.log(typeof myFunction);
+
+
+// Study more
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof#description
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Stack (primitive), Heap (Non-Primitive)  
+
+// STACK
+let myName = "jack"
+let anothername = myName
+anothername = "chaiorcode"
+
+// console.log(myName);
+// console.log(anothername);
+
+// -------------------
+
+// HEAP
+let userOne = {
+    email: "user@ google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "sanket@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
