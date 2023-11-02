1a899af1247e05a869421aeab87b0c6dba3e0a93
Sanket Malvi <sanketmalvi5@gmail.com>
2023-11-02 14:18:05 +0530

    Added Date and Time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,36 @@
+// Dates 
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate = new Date(2023,0, 23)
+// let myCreatedDate = new Date(2023,0, 2, 5, 3)
+// let myCreatedDate = new Date("2023-01-23")
+let myCreatedDate = new Date("01-14-2010")
+
+// console.log(myCreatedDate.toDateString()
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp); //miliseconds
+// console.log(myCreatedDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));  //seconds
+
+
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
